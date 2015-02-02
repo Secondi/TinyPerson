174f3633092b80d5f748b15b3ef2df7f3601d319
Brendon John Muschamp <brendon@numerical.co.nz>
2015-02-02 21:03:34 +1300

    init setup file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='tinyperson',
+    version='0.1.0',
+    packages=['tinyperson', 'tinyperson.screen', 'tinyperson.physics'],
+    url='http://github.com/Secondi/tinyperson',
+    license='MIT',
+    author='Brendon John Muschamp',
+    author_email='brendon@secondi.co.nz',
+    description='Terminal game made by the Secondi jam sessions with drawille and ncurses'
+)
